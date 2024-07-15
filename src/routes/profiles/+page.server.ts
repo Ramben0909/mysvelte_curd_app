ae85f71f4b22c9691aaedbc773f5b868e236fd14
Ritam bhattacharya <ritam07mc@gmail.com>
2024-07-15 22:51:32 +0530

    first commit

--- src/routes/profiles/+page.server.ts
+++ src/routes/profiles/+page.server.ts
@@ -1,4 +1,4 @@
-// import { error } from '@sveltejs/kit';
+import { error } from '@sveltejs/kit';
 import { createPool, sql } from '@vercel/postgres'
 import { POSTGRES_URL } from '$env/static/private'
 
@@ -11,47 +11,43 @@ export async function load() {
       names: names,
     }
   } catch (error) {
-      console.log(
-        'Table does not exist, creating and seeding it with dummy data now...'
-      )
-      // Table is not created yet
-      await seed()
-      const { rows: names } = await db.query('SELECT * FROM names')
-      return {
-        names: names
-      }
-    } 
+    console.log(
+      'Table does not exist, creating and seeding it with dummy data now...'
+    )
+    // Table is not created yet
+    await seed()
+    const { rows: names } = await db.query('SELECT * FROM names')
+    return {
+      names: names
+    }
+  } 
 }
 
 async function seed() {
   const db = createPool({ connectionString: POSTGRES_URL })
   const client = await db.connect();
   const createTable = await client.sql`CREATE TABLE IF NOT EXISTS names (
-      id SERIAL PRIMARY KEY,
-      name VARCHAR(255) NOT NULL,
-      email VARCHAR(255) UNIQUE NOT NULL,
-      "createdAt" TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
-    );
-    `
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    "createdAt" TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+  );`
 
   console.log(`Created "users" table`)
 
   const users = await Promise.all([
     client.sql`
-          INSERT INTO names (name, email)
-          VALUES ('Rohan', 'rohan@tcl.com')
-          ON CONFLICT (email) DO NOTHING;
-      `,
+      INSERT INTO names (name, email)
+      VALUES ('Rohan', 'rohan@tcl.com')
+      ON CONFLICT (email) DO NOTHING;`,
     client.sql`
-          INSERT INTO names (name, email, image)
-          VALUES ('Rebecca', 'rebecca@tcl.com')
-          ON CONFLICT (email) DO NOTHING;
-      `,
+      INSERT INTO names (name, email)
+      VALUES ('Rebecca', 'rebecca@tcl.com')
+      ON CONFLICT (email) DO NOTHING;`,
     client.sql`
-          INSERT INTO names (name, email, image)
-          VALUES ('Vivek', 'vivek@gmail.com')
-          ON CONFLICT (email) DO NOTHING;
-      `,
+      INSERT INTO names (name, email)
+      VALUES ('Vivek', 'vivek@gmail.com')
+      ON CONFLICT (email) DO NOTHING;`
   ])
   console.log(`Seeded ${users.length} users`)
 
@@ -63,22 +59,21 @@ async function seed() {
 
 /** @type {import('./$types').Actions} */
 export const actions = {
-	
-  // update: async ({ request }) => {
-  //   const data = await request.formData();
-  //   const db = createPool({ connectionString: POSTGRES_URL })
-  //   const client = await db.connect();
+  update: async ({ request }) => {
+    const data = await request.formData();
+    const db = createPool({ connectionString: POSTGRES_URL })
+    const client = await db.connect();
 
-  //   const email = data.get('email');
-	// 	const name = data.get('name');
+    const email = data.get('email');
+    const name = data.get('name');
 
-  //   const updateUser = await client.sql`
-  //   UPDATE names
-  //   SET email = ${email}, name = ${name}
-  //   WHERE     ;`
-	
-	// 	return { success: true };
-	// },
+    const updateUser = await client.sql`
+      UPDATE names
+      SET name = ${name}
+      WHERE email = ${email};`
+    
+    return { success: true };
+  },
 
   delete: async ({ request }) => {
     const data = await request.formData();
@@ -88,28 +83,25 @@ export const actions = {
     const id = data.get('id');
 
     const deleteUser = await client.sql`
-    DELETE FROM names
-    WHERE id = ${id};`
-	
-		return { success: true };
-	},
+      DELETE FROM names
+      WHERE id = ${id};`
+    
+    return { success: true };
+  },
 
-	create: async ({request}) => {
-		const data = await request.formData();
+  create: async ({ request }) => {
+    const data = await request.formData();
     const db = createPool({ connectionString: POSTGRES_URL })
     const client = await db.connect();
 
     const email = data.get('email');
-		const name = data.get('name');
+    const name = data.get('name');
 
     const createUser = await client.sql`
       INSERT INTO names (name, email)
       VALUES (${name}, ${email})
-      ON CONFLICT (email) DO NOTHING;
-    `
+      ON CONFLICT (email) DO NOTHING;`
+    
     return { success: true };
-	}
+  }
 };
-
-
-
--- src/routes/profiles/+page.svelte
+++ src/routes/profiles/+page.svelte
@@ -14,18 +14,19 @@
 <div
   class="mt-10 pt-10 w-full max-w-xl p-12 mx-auto rounded-lg shadow-xl dark:bg-white/10 bg-white/30 ring-1 ring-gray-900/5 backdrop-blur-lg"
 >
+  <!-- Create Form -->
   <form method="POST" action="?/create">
     <div class="flex flex-wrap -mx-3 mb-2">
       <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
         <label
           class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-          for="grid-city"
+          for="create-name"
         >
           Name
         </label>
         <input
           class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-          id="name"
+          id="create-name"
           type="text"
           placeholder="Enter name"
           name="name"
@@ -34,23 +35,18 @@
       <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
         <label
           class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-          for="grid-city"
+          for="create-email"
         >
           Email
         </label>
         <input
           class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-          id="email"
+          id="create-email"
           type="text"
           placeholder="Enter email"
           name="email"
         />
       </div>
-      <!-- <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-state">
-              Email
-            </label>
-          </div> -->
       <button
         type="submit"
         class="bg-yellow-500 hover:bg-blue-700 text-white font-bold mt-5 ml-2 px-2 rounded "
@@ -59,9 +55,56 @@
       </button>
     </div>
   </form>
-  {#if form?.success}
-    <!-- this message is ephemeral; it exists because the page was rendered in
-		   response to a form submission. it will vanish if the user reloads -->
+  {#if form?.success && form?.action === 'create'}
     <p class="pt-2">Added new Applicant!</p>
+  {/if}
+</div>
+
+<div
+  class="mt-10 pt-10 w-full max-w-xl p-12 mx-auto rounded-lg shadow-xl dark:bg-white/10 bg-white/30 ring-1 ring-gray-900/5 backdrop-blur-lg"
+>
+  <!-- Update Form -->
+  <form method="POST" action="?/update">
+    <div class="flex flex-wrap -mx-3 mb-2">
+      <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+        <label
+          class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+          for="update-email"
+        >
+          Email
+        </label>
+        <input
+          class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+          id="update-email"
+          type="text"
+          placeholder="Enter email"
+          name="email"
+        />
+      </div>
+      <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+        <label
+          class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+          for="update-name"
+        >
+          Name
+        </label>
+        <input
+          class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+          id="update-name"
+          type="text"
+          placeholder="Enter new name"
+          name="name"
+        />
+      </div>
+      <button
+        type="submit"
+        class="bg-yellow-500 hover:bg-blue-700 text-white font-bold mt-5 ml-2 px-2 rounded "
+      >
+        Update Applicant
+      </button>
+    </div>
+  </form>
+  {#if form?.success && form?.action === 'update'}
+    <p class="pt-2">Updated Applicant!</p>
   {/if}
 </div>
